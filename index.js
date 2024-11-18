497bc5a75bad6fd5a95114d5ef5558392510a0b7
CameronD35 <camster09@ca.rr.com>
2024-11-17 16:55:23 -0800

    Added a bunch of stuff. Ngl I forgot what, but it's nothing insane.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import createHTMLChildElement from "/modules/createElement.js";
 
+const globalSections = [document.getElementById('landingSection'), document.getElementById('locationsSection')]
 
 window.addEventListener('click', (event) => {
 
@@ -38,6 +39,69 @@ window.addEventListener('click', (event) => {
 
 });
 
-document.querySelector('.sideNavbar').addEventListener('mouseenter', () => {
+document.querySelector('.sideNavigation').addEventListener('mouseenter', (event) => {
+    let colorOverlayStyle = document.querySelector('.colorOverlay').style;
+    let navbarStyle = document.querySelector('.sideNavbar').style;
+    let sectionTitleBoxArray = document.querySelectorAll('.sectionTitleBox');
+
     console.log('test');
+    document.querySelector('.sideNavigation').style.transform = 'scale(2)';
+    navbarStyle.boxShadow = '0 0 25px rgba(0, 0, 0, 0.6)';
+    colorOverlayStyle.width = `100vw`;
+    colorOverlayStyle.height = `100vw`;
+    colorOverlayStyle.opacity = 0.3;
+
+    sectionTitleBoxArray.forEach((elem, i) => {
+        elem.style.opacity = 1;
+        elem.addEventListener('mouseenter', detectHoverOnSectionText);
+        elem.addEventListener('mouseleave', detectExitSectionText);
+    });
+
+
+});
+
+document.querySelector('.sideNavigation').addEventListener('mouseleave', (event) => {
+    let colorOverlayStyle = document.querySelector('.colorOverlay').style;
+    let sectionTitleBoxArray = document.querySelectorAll('.sectionTitleBox');
+
+    console.log('test');
+    document.querySelector('.sideNavigation').style.transform = '';
+    event.target.style.boxShadow = ''
+    colorOverlayStyle.width = `0`;
+    colorOverlayStyle.height = `0`;
+    colorOverlayStyle.opacity = 0;
+
+    sectionTitleBoxArray.forEach((elem, i) => {
+        elem.style.opacity = 0;
+        elem.removeEventListener('mouseenter', detectHoverOnSectionText);
+        elem.removeEventListener('mouseleave', detectExitSectionText);
+    });
+});
+
+function detectHoverOnSectionText(event){
+    //event.target.children[0].style.color = 'var(--eurekaGreen)';
+    event.target.children[0].style.scale = '2';
+}
+
+function detectExitSectionText(event){
+    //event.target.children[0].style.color = 'var(--eurekaWhite)';
+    event.target.children[0].style.scale = '1';
+}
+
+function detectForCurrentSection(){
+    globalSections.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            console.log('yoooo');
+        })
+    })
+}
+
+const observer = new IntersectionObserver((entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting){
+            console.log('it is rendered')
+        }
+    }
 })
+
+detectForCurrentSection()
